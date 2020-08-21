9160bc2bb036c38f3b5b3d3d7dec8525158da089
Ilya Boyko <42005828+bkilya@users.noreply.github.com>
2020-08-21 10:09:44 +0300

    Fixing incorrect content-type setting in the deploy script

--- deploy/deployBuild.ps1
+++ deploy/deployBuild.ps1
@@ -1,8 +1,8 @@
-$subscriptionId = "fc7ebf7d-b3ae-45c2-9c08-67d0ae8ce012"
-$resourceGroupName = "nibeauli-teams-lms-test-1"
-$storageAccountName = "nibeaulilmsstorage01"
+$subscriptionId = "2ead6133-2cc0-4269-b52f-e653fb4c3dae"
+$resourceGroupName = "TeamsCreateMeetingApp"
+$storageAccountName = "teamscreatemeetingcustom"
 
-# Connect-AzAccount
+if($context -eq $null){ Connect-AzAccount}
 
 $context = Get-AzSubscription -SubscriptionId $subscriptionId
 Set-AzContext $context
@@ -19,3 +19,35 @@ Get-ChildItem -Path ../build -File -Recurse | Set-AzStorageBlobContent `
 -Properties @{ ContentType = "text/html; charset=utf-8" } `
 -Context $ctx `
 -Force
+
+$blobs = Get-AzStorageBlob -Container "`$web" -Context $ctx
+foreach ($blob in $blobs)
+{
+    $CloudBlockBlob = [Microsoft.Azure.Storage.Blob.CloudBlockBlob] $Blob.ICloudBlob
+    $CloudBlockBlob.FetchAttributes()
+    if($blob.Name -like "*.svg")
+    {
+        $CloudBlockBlob.Properties.ContentType = "image/svg+xml"
+    }elseif($blob.Name -like "*.json")
+    {
+        $CloudBlockBlob.Properties.ContentType = "application/json"
+    }elseif($blob.Name -like "*.css")
+    {
+        $CloudBlockBlob.Properties.ContentType = "text/css"
+    }
+    elseif($blob.Name -like "*.ico")
+    {
+        $CloudBlockBlob.Properties.ContentType = "image/x-icon"
+    }
+    elseif($blob.Name -like "*.js")
+    {
+        $CloudBlockBlob.Properties.ContentType = "text/javascript"
+    }elseif($blob.Name -like "*.txt")
+    {
+        $CloudBlockBlob.Properties.ContentType = "text/plain"
+    }elseif($blob.Name -like "*.map")
+    {
+        $CloudBlockBlob.Properties.ContentType = "text/plain"
+    }
+    $CloudBlockBlob.SetProperties()
+}
